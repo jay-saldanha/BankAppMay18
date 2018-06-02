a2e1055c694738f487f1aafed0f7fb447ac9212f
Jazz4Code <Jayana@DESKTOP-REO9QJL>
2018-06-02 00:56:22 -0700

    Initial Commit

---
+++ BankApp1/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp1
+{
+    /// <summary>
+    /// Bank acoount 
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Unique Account Number for the classs
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// customer email adress corresponding to Account number
+        /// </summary>
+        public String EmailAddress { get; set; }
+        /// <summary>
+        /// Balance in the account 
+        /// </summary>
+        public decimal Balance { get; set; }
+        /// <summary>
+        /// The type of account 
+        /// </summary>
+        public string TypeOfAccount { get; set; }
+        #endregion
+    }
+}
